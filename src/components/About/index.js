7c5ece717d3c814d106172bfc7e439bb90b3855a
Revanth1997 <revanth1997.rn@gmail.com>
2021-11-19 11:59:00 +0000

    af

---
+++ src/components/About/index.js
@@ -0,0 +1,15 @@
+import Header from '../Header'
+import LogoutButton from '../LogoutButton'
+import './index.css'
+
+const About = () => (
+  <>
+    <Header />
+    <div className="about-container">
+      <h1 className="heading">About Route</h1>
+    </div>
+    <LogoutButton />
+  </>
+)
+
+export default About
---
+++ src/components/Header/index.js
@@ -0,0 +1,23 @@
+import {Link} from 'react-router-dom'
+import './index.css'
+
+const Header = () => (
+  <nav className="nav-header">
+    <div className="nav-content">
+      <ul className="nav-menu">
+        <li>
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/about" className="nav-link">
+            About
+          </Link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+)
+
+export default Header
